67f6905a87d5b8b2cb951b10420a1ccb9abf1b4c
Mery <wangmeryl77@gmail.com>
2023-03-20 03:06:39 -0400

    adjust component heights

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
-import UserFinder from './components/UserFinder';
-import RepoBoard from './components/RepoBoard';
+import UserFinder from "./components/UserFinder";
+import RepoBoard from "./components/RepoBoard";
 
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Grid from '@mui/material/Grid';
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Grid from "@mui/material/Grid";
 
+import { blueGrey, red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
 
-import { blueGrey, red } from '@mui/material/colors';
-import { createTheme } from '@mui/material/styles';
-
-// TODO: include option to view all repos 
-// button View All Repos -> pass flag to use repos/ endpoint 
-// todo: add light/dark mode 
+// TODO: add light/dark mode
 function App() {
-
   const theme = createTheme({
     palette: {
       primary: {
         light: blueGrey.A100,
         main: blueGrey.A400,
         dark: blueGrey.A700,
-        contrastText: '#fff',
+        contrastText: "#fff",
       },
       secondary: {
         light: red[50],
         main: red[100],
         dark: red[900],
-        contrastText: '#000',
+        contrastText: "#000",
       },
     },
   });
@@ -42,42 +38,64 @@ function App() {
   const [selectedUsername, setSelectedUsername] = useState("");
 
   const setUsername = (username) => {
-    console.log('calling setUsername')
-    setSelectedUsername(username)
-  }
+    console.log("calling setUsername");
+    setSelectedUsername(username);
+  };
 
   return (
-    // TODO center box 
-    <Box sx={{ 
-      width: '100%', 
-      height: '100%',
-      bgcolor: 'background.paper', 
-      flexGrow:1
-      }}>
-
-      <AppBar position="static" color='primary'>
-      <Toolbar>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        bgcolor: "background.default",
+        flexGrow: 1,
+      }}
+    >
+      <AppBar position="static" color="primary">
+        <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Github Repo Explorer
           </Typography>
         </Toolbar>
       </AppBar>
 
-      <Grid container spaing={2} padding={5}>
-        <Grid item xs={4} padding={2}>
-          <UserFinder 
-            setUsername={setUsername}
-          />
+      <Grid
+        container
+        spaing={2}
+        padding={5}
+        sx={{
+          height: "auto",
+          maxHeight: "75%",
+          overflow: "scroll",
+        }}
+      >
+        <Grid
+          item
+          xs={4}
+          padding={2}
+          sx={{
+            height: "auto",
+            maxHeight: 700,
+            overflow: "scroll",
+          }}
+        >
+          <UserFinder setUsername={setUsername} />
         </Grid>
 
-        <Grid item xs={8} padding={2}>
-          <RepoBoard 
-            selectedUsername={selectedUsername}
-          />
+        <Grid
+          item
+          xs={8}
+          padding={2}
+          sx={{
+            height: "auto",
+            maxHeight: 700,
+            overflow: "scroll",
+          }}
+        >
+          <RepoBoard selectedUsername={selectedUsername} />
         </Grid>
       </Grid>
     </Box>
-
   );
 }
 
--- src/components/RepoBoard.js
+++ src/components/RepoBoard.js
@@ -11,62 +11,61 @@ import RepoCard from './RepoCard';
 
 
 function RepoBoard(props) {
-    const [repos, setRepos] = useState([]);
-    const selectedUsername = props.selectedUsername;
+  const [repos, setRepos] = useState([]);
+  const selectedUsername = props.selectedUsername;
 
-    // TODO: put this in ENV 
-    const BASE_URL ='https://aqueous-journey-63498.herokuapp.com';
+  // TODO: put this in ENV 
+  const BASE_URL = 'https://aqueous-journey-63498.herokuapp.com';
 
 
-    // fetch data of repos to be displayed 
-    useEffect(() => {
-        // TODO: update this api, use props.selectedUsername to construct api endpoint
-        // const REPOS_API = 'http://localhost:8000/repos/'
-        // TODO: display all or no repos if no username selected 
-        if (selectedUsername) {
-            // const REPOS_API = `http://localhost:8000/users/${selectedUsername}/repos`
-            const REPOS_API = `${BASE_URL}/users/${selectedUsername}/repos`;
-            fetch(REPOS_API)
-                .then((res) => res.json())
-                .then((json) => {
-                    setRepos(json)
-                })
-        } else {
-            // reset board if username selection removed 
-            setRepos([])
-        }
-    }, [selectedUsername])
+  // fetch data of repos to be displayed 
+  useEffect(() => {
+    // TODO: update this api, use props.selectedUsername to construct api endpoint
+    // const REPOS_API = 'http://localhost:8000/repos/'
+    // TODO: display all or no repos if no username selected 
+    if (selectedUsername) {
+      // const REPOS_API = `http://localhost:8000/users/${selectedUsername}/repos`
+      const REPOS_API = `${BASE_URL}/users/${selectedUsername}/repos`;
+      fetch(REPOS_API)
+        .then((res) => res.json())
+        .then((json) => {
+          setRepos(json)
+        })
+    } else {
+      // reset board if username selection removed 
+      setRepos([])
+    }
+  }, [selectedUsername])
 
-    return (
-        // TODO implement scrolling
-        <Box sx={{ 
-            width:'auto',
-            height:'auto',
-            maxWidth:'100%',
-            bgcolor: 'background.paper', 
-            boxShadow:1, 
-            overflow:'scroll'
-            }}
-        >
-            <Typography 
-                color="text.secondary" 
-                gutterBottom
-                paddingLeft={2}
-                paddingTop={2}
-            >
-                Repos
-            </Typography>  
-            <Grid container spaing={2}>
-                {/* TODO: reduce number of cards per row when window width decreases */}
-                {repos.map((repo) => (
-                    <Grid item xs={6} padding={2}>
-                         <RepoCard repo={repo} />
-                    </Grid>
-                ))}
-            </Grid>
-            
-        </Box>
-    )
+  return (
+    // TODO implement scrolling
+    <Box sx={{
+      width: 'auto',
+      height: 'auto',
+      maxWidth: '100%',
+      maxHeight: '100%',
+      bgcolor: 'background.default',
+      boxShadow: 1,
+      overflow: 'scroll'
+    }}
+    >
+      <Typography
+        color="text.secondary"
+        gutterBottom
+        paddingLeft={2}
+        paddingTop={2}
+      >
+        Repos
+      </Typography>
+      <Grid container spaing={2}>
+        {repos.map((repo) => (
+          <Grid item xs={6} padding={2}>
+            <RepoCard repo={repo} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  )
 
 }
 
